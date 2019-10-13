53f13dfb3268a52ad8c4027cdd3881a107147b59
Peter_shw <787297031@qq.com>
2019-10-13 22:46:46 +0800

    修改配置中心uri为gitlab

---
+++ client-your/src/main/java/com/shw/clientyour/ClientYourApplication.java
@@ -0,0 +1,28 @@
+package com.shw.clientyour;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ClientYourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientYourApplication.class, args);
+    }
+
+    @Value("${app.environment}")
+    String environment;
+
+    @Value("${my.name}")
+    String name;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return environment + "," +name;
+    }
+
+}
